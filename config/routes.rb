78279e977b86037445a5617512e5ef8df193d559
Alyssa Hursh <akhursh@gmail.com>
2016-10-23 19:34:35 -0700

    Added sessions controller and user in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get "/auth/:provider/callback" =>  "sessions#create"
-
+  resources :sessions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
